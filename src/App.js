cc61de477d267557cb2d7ca17192003c8d1efd60
Maria Jesus Bellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-09 19:44:22 +0100

    Fixing typos

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import axios from "axios";
 export default function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0d035b97a9d8883de99fce56852edb02";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0d035b97a9d8883de99fce56852edb02`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
