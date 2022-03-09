c165e16c0b7521a10e9dae24d2021dcf2cfa50c1
Maria Jesus Bellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-09 20:30:07 +0100

    refactoring code to show metric units

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export default function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0d035b97a9d8883de99fce56852edb02`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=0d035b97a9d8883de99fce56852edb02`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -34,22 +34,18 @@ export default function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? (
-              <h1>{Math.round(data.main.temp - 273.15)}ºC</h1>
-            ) : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}ºC</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
-        {data.name != undefined && (
+        {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
               {data.main ? (
-                <p className="bold">
-                  {Math.round(data.main.feels_like - 273.15)}ºC
-                </p>
+                <p className="bold">{data.main.feels_like.toFixed()}ºC</p>
               ) : null}
               <p>Feels like</p>
             </div>
@@ -59,7 +55,7 @@ export default function App() {
             </div>
             <div className="wind">
               {data.wind ? (
-                <p className="bold">{(data.wind.speed * 1.6).toFixed(2)} KPH</p>
+                <p className="bold">{data.wind.speed.toFixed} KPH</p>
               ) : null}
               <p>Wind Speed </p>
             </div>
