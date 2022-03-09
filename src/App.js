8ab63314580801a47c20fee140f470ab1716c3b8
Maria Jesus Bellido <54066498+mariajbellido@users.noreply.github.com>
2022-03-09 13:44:01 +0100

    adding files and first styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import "./styles.css";
 import axios from "axios";
 
 export default function App() {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=0d035b97a9d8883de99fce56852edb02";
+  //const url = "https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=0d035b97a9d8883de99fce56852edb02";
 
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Barcelona</p>
+          </div>
+          <div className="temp">
+            <h1>Temperature</h1>
+          </div>
+          <div className="description">
+            <p>Description</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>Feels</p>
+          </div>
+          <div className="humidity">
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p>Wind</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
